e3403c957df81ff687f03127d200776f38a225ac
agatasnoske40122 <agatasnoske40122@gmail.com>
2026-05-28 11:07:31 +0900

    Contact 調整

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,12 @@
 import './Contact.scss'
 
 function Contact() {
-  return (
-    <div className="main fade">
-        <div className="contactWrap">
-            <p className="txt">現在準備中です。<br />お問い合わせはこちらにご連絡ください</p>
-            <p className="mail">agatasnoske40122@gmail.com</p>
+    return (
+        <div className="main fade">
+            <div className="contactWrap">
+                <p className="txt">現在準備中です。</p>
+            </div>
         </div>
-    </div>
-)
+    )
 }
 export default Contact
